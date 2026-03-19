5763bee29c913dad4b64f49ec12fe8481758fdf2
BranchCrypto <BranchCrypto@users.noreply.github.com>
2026-03-19 14:07:33 +0800

    Remove last temp script

--- _cleanup2.bat
+++
@@ -1,7 +0,0 @@
-set HTTPS_PROXY=http://127.0.0.1:7890
-set HTTP_PROXY=http://127.0.0.1:7890
-cd C:\Users\Administrator\Desktop\apikey-manager
-del _cleanup.bat
-git add -A
-git commit -m "Remove last temp script"
-git push 2>&1
